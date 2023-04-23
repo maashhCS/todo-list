546f0c2da02105e0d76c067adaf1ca65dfffcb0f
maashh <58293726+maashhCS@users.noreply.github.com>
2023-04-23 21:08:11 +0200

    removed second default project

--- src/storage_updater.js
+++ src/storage_updater.js
@@ -9,7 +9,6 @@ export function updateLocalStorage(){
 export function getLocalStorage(){
     if (!localStorage.getItem('projects')) {
         projectArray.push(createProject('Default'));
-        projectArray.push(createProject('Default'))
         projectArray[0].projectTaskArray.push(createTask('Title', 'Description', 'low', '2023-04-21', false));
     } else {
         const projectsLocalStorage = JSON.parse(localStorage.getItem('projects'));
